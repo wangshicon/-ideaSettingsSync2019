3f29a1a4feb889a584978c580b33b4ee9839c81b
Lord <1334996110@qq.com>
2020-04-22 11:22:50 +0800

    IU-2019.3.3 <Lord@DESKTOP-DUJDIMN Create ide.general.xml, git.xml, project.default.xml, rainbow_brackets.xml, Enum.java, Interface.java, Default.xml, terminal.xml, Default.xml, GoogleStyle.xml, ignore.xml, code.style.schemes, ui.lnf.xml, qaPlugPluginUpdateNotifier.xml, code.style.schemes.xml, colors.scheme.xml, mavenVersion.xml, AnnotationType.java, javarebel.xml, GrepConsole.xml, laf.xml, Class.java, jdk.table.xml, path.macros.xml, wsl.distributions.xml, codota.xml, mybatis.xml, editor.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,10 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ * TODO Description
+ *
+ * @author ${USER}
+ * @date ${YEAR}/${MONTH}/${DAY}
+ */
+public class ${NAME} {
+}
